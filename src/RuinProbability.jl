7d4c7b64f9b4998db3d781ed8d1cab9e5a4360f3
Hank-Qian <joeyuehank@gmail.com>
2014-11-30 13:20:01 +0000

    Update RuinProbability.jl

--- src/RuinProbability.jl
+++ src/RuinProbability.jl
@@ -52,32 +52,32 @@ function EMfit(claims_data)
        sumA = zeros(1,3);
        
        for n = 1:300;
-       for j =1:3;
-       for i =1:344;
-       
-              w[i,j] = P[j] * (1 / Theta[1,j]) * exp(-claims_data[i] / Theta[1,j]) / (P[1] * (1 / Theta[1,1]) * exp(-claims_data[i] / Theta[1,1]) + P[1,2] * (1 / Theta[1,2]) * 
-       exp(-claims_data[i] / Theta[1,2]) + P[3] * (1 / Theta[1,3]) * exp (-claims_data[i] / Theta[1,3]))
-       end;
-       end;
-       for m=1:3;
-       for k=1:344;
-       Q[k,m] = w[k,m] * claims_data[k];
-       A[k,m] = w[k,m];
-       end;
-       Theta[1,m] = sum(Q[:,m]) / sum(A[:,m]); 
-       P[1,m] = sum(A[:,m]) / 344;
-       end;
+              for j =1:3;
+                     for i =1:344;
+                     
+                            w[i,j] = P[j] * (1 / Theta[1,j]) * exp(-claims_data[i] / Theta[1,j]) / (P[1] * (1 / Theta[1,1]) * exp(-claims_data[i] / Theta[1,1]) + P[1,2] * (1 / Theta[1,2]) * 
+                            exp(-claims_data[i] / Theta[1,2]) + P[3] * (1 / Theta[1,3]) * exp (-claims_data[i] / Theta[1,3]))
+                     end;
+              end;
+              for m=1:3;
+                     for k=1:344;
+                            Q[k,m] = w[k,m] * claims_data[k];
+                            A[k,m] = w[k,m];
+                     end;
+                     Theta[1,m] = sum(Q[:,m]) / sum(A[:,m]); 
+                     P[1,m] = sum(A[:,m]) / 344;
+              end;
        end;
        Alpha=zeros(3);
        P_a=zeros(3);
        for n=1:3;
-       Alpha[n]=1/Theta[n];
-       P_a[n]=P[n];
+              Alpha[n]=1/Theta[n];
+              P_a[n]=P[n];
        end;
        return (P_a, Alpha);
        end
        
-       end
+
        
 function SPMixExp(surplusprocess) 
        Alpha=zeros(3);
