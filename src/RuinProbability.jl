98e2865939a66d14febb5cc12904570410cdc0ed
Hank-Qian <joeyuehank@gmail.com>
2014-12-03 17:21:35 +0000

    Update RuinProbability.jl

--- src/RuinProbability.jl
+++ src/RuinProbability.jl
@@ -27,17 +27,12 @@ end
 # expense_rati=0.2
 # duration=10 ( the unit of duration is year)
 #-------------------------------------------------------------------------------
-
+include("SPExponential.jl");
+include("SPMix3Exponential.jl");
+include("QQPlot.jl");
+include("EMfit");
+include("SPFG");
 end # module
-
-function SPExp(surplusprocess)
-       leng = length(claims_data);
-       aver = mean(claims_data);
-       s= 1 - (1 - loss_ratio/expense_ratio)* (leng /duration) * exp(-(1/ (aver)- (leng/duration) /( (leng/duration) * aver * (expense_ratio/loss_ratio))) * initial_capital)/(1/aver*(leng/duration)*aver*expense_ratio/(loss_ratio) - (leng/duration));
-end
-#-------------------------------------------------------------------------------
-# This result is generated from surplus process with exponential claims distribution and claims size is distributed by poison process. The function is produced by Laplace transform and inverse Laplace transform from IE to IDE to the final result.   
-#-------------------------------------------------------------------------------
 
 
 function EMfit(claims_data)
