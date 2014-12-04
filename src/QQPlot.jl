5bb511a0757bedc67840e441f898d0c5e6fffd23
Hank-Qian <joeyuehank@gmail.com>
2014-12-04 16:19:17 +0000

    Update QQPlot.jl

--- src/QQPlot.jl
+++ src/QQPlot.jl
@@ -1,30 +1,31 @@
 function QQPlot(claims_data)
+leng=length(claims_data)
        for n=1:3;
               Alpha[n]=EMfit(claims_data)[2][n]
               P_a[n]= EMfit(claims_data)[1][n]
        end;
        w=rand(Distributions.Exponential(aver),100000);
-       percentile=rand(344);
-       for i=1:344;
-            percentile[i]=i/344;
+       percentile=rand(leng);
+       for i=1:leng;
+            percentile[i]=i/leng;
        end;
-       QQ_1=rand(344); 
-       for i=1:344;
+       QQ_1=rand(leng); 
+       for i=1:leng;
             QQ_1[i]=quantile(w,percentile[i]);
        end;
        w=sort(claims_data);
-       QQ_3=zeros(344);
-       F=zeros(344);
-       F_d=zeros(344);
-       for i=1:344;
+       QQ_3=zeros(leng);
+       F=zeros(leng);
+       F_d=zeros(leng);
+       for i=1:leng;
               F[i]=P_a[1] *(1 - exp(-Alpha[1] * QQ_3[i]))+ P_a[2] * (1 - exp(-Alpha[2] * QQ_3[i])) + P_a[3] * (1 - exp(-Alpha[3] * QQ_3 [i])) -percentile[i];
               F_d[i]= P_a[1] * Alpha[1]  * exp(-Alpha[1] * QQ_3[i]) + P_a[2] * Alpha[2]  * exp(-Alpha[2] * QQ_3 [i]) + P_a[3] * Alpha[3]  * exp(-Alpha[3] * QQ_3 [i]);
        end;
               for n=1:10;
-                     for i=1:344;
+                     for i=1:leng;
                      QQ_3[i]=QQ_3[i] - F[i]/F_d[i];
                      end;
-                     for i=1:344;
+                     for i=1:leng;
                      F[i]=P_a[1] *(1 - exp(-Alpha[1] * QQ_3[i]))+ P_a[2] * (1 - exp(-Alpha[2] * QQ_3[i])) + P_a[3] * (1 - exp(-Alpha[3] * QQ_3 [i])) -percentile[i];
                      F_d[i]= P_a[1] * Alpha[1]  * exp(-Alpha[1] * QQ_3[i]) + P_a[2] * Alpha[2]  * exp(-Alpha[2] * QQ_3 [i]) + P_a[3] * Alpha[3]  * exp(-Alpha[3] * QQ_3 [i]);
                      end;
