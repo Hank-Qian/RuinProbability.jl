aca0360511364abbfd4bb783e8c2286126bf2ef8
Hank-Qian <joeyuehank@gmail.com>
2014-12-04 17:02:59 +0000

    Update SPFractionGamma.jl

--- src/SPFractionGamma.jl
+++ src/SPFractionGamma.jl
@@ -1,19 +1,3 @@
-type surplusprocess
-       initial_capital::Number;
-       claims_data::Array{Float64,1};
-       loss_ratio::Float64;
-       expense_ratio::Float64;
-       duration::Number;
-end
-#-------------------------------------------------------------------------------
-# User needs to input some basic information about the company.
-# For example, user needs to type:
-# initial_capital=10000
-# claims_data=Array ( which can be plugin by collecting from text file, and the data should be only a row without title)
-# loss_ratio=0.67
-# expense_rati=0.2
-# duration=10 ( the unit of duration is year)
-#-------------------------------------------------------------------------------
 function SPFG(surplusprocess)
 #Gamma Distribution Fitting by MLE
 a = 0.5/(log(mean(claims_data)) - mean(log(claims_data)));
