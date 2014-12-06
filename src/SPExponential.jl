842fc3f897fd9bf3f4333e8ac16afcf40bd8c1ea
Hank-Qian <joeyuehank@gmail.com>
2014-12-06 19:09:58 +0000

    Update SPExponential.jl

--- src/SPExponential.jl
+++ src/SPExponential.jl
@@ -2,7 +2,7 @@ function SPExp(sp::SurplusProcess)
        leng = length(sp.claims_data);
        aver = mean(sp.claims_data);
        
-       s = 1 - (1 - sp.loss_ratio / sp.expense_ratio) * leng / sp.duration * exp(-(1/ aver- leng / sp.duration / ( leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio) * sp.initial_capital ) / (1 / aver * leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio - leng / sp.duration);
+       s = 1 - (1 - sp.loss_ratio / sp.expense_ratio) * leng / sp.duration * exp(-(1 / aver - leng / sp.duration / ( leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio) * sp.initial_capital ) / (1 / aver * leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio - leng / sp.duration));
 end
 #-------------------------------------------------------------------------------
 # This result is generated from surplus process with exponential claims distribution and 
