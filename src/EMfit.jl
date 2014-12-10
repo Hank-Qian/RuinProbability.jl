b59cd3b184da6fa8d4647016574ce47033db091f
Hank-Qian <joeyuehank@gmail.com>
2014-12-10 00:33:04 +0000

    Update EMfit.jl

--- src/EMfit.jl
+++ src/EMfit.jl
@@ -1,36 +1,39 @@
-function EMfit(claims_data)
-leng=length(claims_data)
-       Theta = [100000. 100000. 100000.]
-       P = [0.5 0.4 0.1]
-       w = zeros(leng,3)
-       Q = zeros(leng,3)
-       A = zeros(leng,3)
-       sumQ = zeros(1,3)
-       sumA = zeros(1,3)
+function EMfit(claims_data,n::Number)
+       maxc=sort(claims_data)[end]
+       leng=length(claims_data)
+       Theta = rand(0.5*maxc:0.8*maxc,nexp);
+       P = ones(nexp)/nexp;
+       w = zeros(leng,nexp);
+       Q = zeros(leng,nexp);
+       A = zeros(leng,nexp);
+       sumQ = zeros(1,nexp); 
+       sumA = zeros(1,nexp);
+       De=0;
        
-       for n = 1:300
-              for j =1:3
-                     for i =1:leng
-                            w[i,j] = P[j] * (1 / Theta[1,j]) * exp(-claims_data[i] / Theta[1,j]) / (P[1] * (1 / Theta[1,1]) * exp(-claims_data[i] / Theta[1,1]) + P[2] * (1 / Theta[1,2]) * exp(-claims_data[i] / Theta[1,2]) + P[3] * (1 / Theta[1,3]) * exp (-claims_data[i] / Theta[1,3]))
-                     end
-              end
-              
-              for m=1:3
-                     for k=1:leng
-                            Q[k,m] = w[k,m] * claims_data[k]
-                            A[k,m] = w[k,m]
-                     end
-                     Theta[1,m] = sum(Q[:,m]) / sum(A[:,m])
-                     P[1,m] = sum(A[:,m]) / 344
-              end
-       end
-       
-       Alpha=zeros(3)
-       P_a=zeros(3)
-       
-       for n=1:3
-              Alpha[n]=1/Theta[n]
-              P_a[n]=P[n]
-       end
-       return (P_a, Alpha)
+       for n = 1:300;
+              for j =1:nexp;
+                     for i =1:leng;
+                     De=0;
+                            for z=1:nexp
+                            De=P[z] * (1 / Theta[z]) * exp(-claims_data[i] / Theta[z]) +De
+                            end;
+                     w[i,j] = P[j] * (1 / Theta[j]) * exp(-claims_data[i] / Theta[j]) / De
+                     end;
+              end;
+              for m=1:nexp;
+                     for k=1:leng;
+                     Q[k,m] = w[k,m] * claims_data[k];
+                     A[k,m] = w[k,m];
+                     end;
+              Theta[m] = sum(Q[:,m]) / sum(A[:,m]); 
+              P[m] = sum(A[:,m]) / leng;
+              end;
+       end;
+       Alpha=zeros(nexp);
+       P_a=zeros(nexp);
+       for n=1:nexp;
+              Alpha[n]=1/Theta[n];
+              P_a[n]=P[n];
+       end;
+       return (P_a, Alpha);
        end
