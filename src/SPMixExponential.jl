576882c4f20efd7bddeb5b8a8fa905a9d3373dae
Hank-Qian <joeyuehank@gmail.com>
2015-01-05 00:05:21 +0000

    Update SPMixExponential.jl

--- src/SPMixExponential.jl
+++ src/SPMixExponential.jl
@@ -4,7 +4,7 @@ function SPMixExp(sp::SurplusProcess,nexp::Int64)
 	
        leng = length(sp.claims_data);
        aver = mean(sp.claims_data);
-       AA=EMfit(sp.claims_data,nexp);
+       AA=EMfit(sp,nexp);
        for n=1:nexp;
               Alpha[n] = AA[2][n];
               P_a[n] = AA[1][n];
