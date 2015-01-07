01aed63fe8f9d67a55438222761dd215de4eb2ba
Hank-Qian <joeyuehank@gmail.com>
2015-01-07 23:22:36 +0000

    Update DSum.jl

--- src/DSum.jl
+++ src/DSum.jl
@@ -29,8 +29,8 @@ function DSum(sp::SurplusProcess, nexp::Int64)
 	S3=0;
        pop!(QQ_1)
        pop!(w)
-        S1=abs(sum(w)-sum(QQ_1);
-        S3=abs(sum(w)-sum(QQ_3);
+        S1=abs(sum(w)-sum(QQ_1));
+        S3=abs(sum(w)-sum(QQ_3));
         
         Exp=S1/(leng-1);
         FG=S3/(leng-1);
@@ -69,7 +69,7 @@ function DSum(sp::SurplusProcess, nexp::Int64)
        
 	
       pop!(QQ_m);
-       S2=abs(sum(w)-sum(QQ_m);
+       S2=abs(sum(w)-sum(QQ_m));
       
        MixExp=S2/(leng-1);
        ModelName=["Exp", "MixExp", "FG"];
