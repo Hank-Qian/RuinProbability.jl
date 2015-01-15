09496dbcbbd29a273ab26a1863983a25c58c73d2
Hank-Qian <joeyuehank@gmail.com>
2015-01-15 21:06:55 +0000

    Update Simulation.jl

--- src/Simulation.jl
+++ src/Simulation.jl
@@ -1,10 +1,9 @@
 function Simulation(sp::SurplusProcess, u_0::Number,years::Number,D = ASCIIString)
+steps=3000;
 leng=length(sp.claims_data);
 aver=mean(sp.claims_data);
 In_time=Distributions.Exponential(sp.duration/leng);
-x=Distributions.Exponential(mean(sp.claims_data));
-claim=rand(x,3000);
-time=rand(In_time,3000);
+time=rand(In_time,steps);
 T=years;
 t_0=0;
 r=0;
@@ -12,11 +11,13 @@ nr=0;
 c = leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio; 
 if D == "Exponential"
 if c > aver
+x=Distributions.Exponential(mean(sp.claims_data));
+claim=rand(x,steps);
 for n=1:10000;
 ut=u_0;
 t=t_0;
 i=0;
-while i<3000 && ut>0 && t<T;
+while i<steps && ut>0 && t<T;
 i = i + 1;
 ut = ut + leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio * time[i] - claim[i];
 t = t + time[i];
@@ -26,8 +27,8 @@ r = r + 1;
 else;
 nr = nr + 1;
 end;
-time=rand(In_time,3000);
-claim=rand(x,3000);
+time=rand(In_time,steps);
+claim=rand(x,steps);
 end;
 s = nr/(r+nr);
 else c < aver
@@ -37,7 +38,6 @@ end
   
 if D == "MixExponential"
 if c > aver
-steps=3000
 unif=rand(Uniform(), steps)
 Alpha = zeros(3);
 P_a = zeros(3);
@@ -89,6 +89,36 @@ s = nr/(nr+r)
 else c < aver
 println("Wrong information about loss ratio and expense ratio")
 end
-end 
+end
+
+
+if D == "FracGamma"
+if c > aver
+a = 0.5/(log(mean(sp.claims_data)) - mean(log(sp.claims_data)));
+b = mean(sp.claims_data)/a;
+x=Distributions.Gamma(a,b);
+claim=rand(x,steps);
+for n=1:10000;
+ut=u_0;
+t=t_0;
+i=0;
+while i<steps && ut>0 && t<T;
+i = i + 1;
+ut = ut + leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio * time[i] - claim[i];
+t = t + time[i];
+end;
+if t < T;
+r = r + 1;
+else;
+nr = nr + 1;
+end;
+time=rand(In_time,3000);
+claim=rand(x,3000);
+end;
+s = nr/(r+nr);
+else c < aver
+println("Wrong information about loss ratio and expense ratio")
+end
+end
   return(s)
 end
