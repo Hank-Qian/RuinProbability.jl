e00f64862c8b4ef46d26fdf19488e8535b30ce9c
Hank-Qian <joeyuehank@gmail.com>
2015-01-21 00:57:40 +0000

    Update Abbreviation.jl

--- src/Abbreviation.jl
+++ src/Abbreviation.jl
@@ -1,26 +1,3 @@
 Exp="Exponential";
 MixExp="MixExponential";
 FG="FracGamma";
-
-function Simulation(u_0::Number,years::Number,D = ASCIIString)
-  Simulationn(SP,u_0::Number,years::Number,D = ASCIIString)
-end
-function EMfit(nexp::Number)
-  EMfitt(SP,nexp::Number)
-end
-function PDSum(nexp::Int64)
-  PDSumm(SP, nexp::Int64)
-end
-function PPPlot(nexp::Int64)
-  PPPlott(SP, nexp::Int64)
-end
-function PlotSP(samples::Number)
-  PlotSPP(SP,samples::Number)
-end
-function QQPlot(nexp::Int64)
-  QQPlott(SP, nexp::Int64)
-end
-function SurPro(D::ASCIIString)
-  SurProo(SP, D::ASCIIString)
-end
-
