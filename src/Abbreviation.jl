f97182aff780e3e8c4e9087c9b3e81bc4a4b4aab
Hank-Qian <joeyuehank@gmail.com>
2015-01-21 00:53:41 +0000

    Update Abbreviation.jl

--- src/Abbreviation.jl
+++ src/Abbreviation.jl
@@ -3,24 +3,24 @@ MixExp="MixExponential";
 FG="FracGamma";
 
 function Simulation(u_0::Number,years::Number,D = ASCIIString)
-  Simulationn(SP::SurplusProcess,u_0::Number,years::Number,D = ASCIIString)
+  Simulationn(SP,u_0::Number,years::Number,D = ASCIIString)
 end
 function EMfit(nexp::Number)
-  EMfitt(SP::SurplusProcess,nexp::Number)
+  EMfitt(SP,nexp::Number)
 end
 function PDSum(nexp::Int64)
-  PDSumm(SP::SurplusProcess, nexp::Int64)
+  PDSumm(SP, nexp::Int64)
 end
 function PPPlot(nexp::Int64)
-  PPPlott(SP::SurplusProcess, nexp::Int64)
+  PPPlott(SP, nexp::Int64)
 end
 function PlotSP(samples::Number)
-  PlotSPP(sp::SurplusProcess,samples::Number)
+  PlotSPP(SP,samples::Number)
 end
 function QQPlot(nexp::Int64)
-  QQPlott(sp::SurplusProcess, nexp::Int64)
+  QQPlott(SP, nexp::Int64)
 end
 function SurPro(D::ASCIIString)
-  SurProo(sp::SurplusProcess, D::ASCIIString)
+  SurProo(SP, D::ASCIIString)
 end
 
