72a678cafbd383297fe530db3407c1fa99ac845b
Hank-Qian <joeyuehank@gmail.com>
2015-03-02 17:25:24 +0800

    Update SurPro.jl

--- src/SurPro.jl
+++ src/SurPro.jl
@@ -4,7 +4,7 @@ function SurPro(sp::SurplusProcess,u_0::Number, D::ASCIIString)
        c = leng / sp.duration * aver; 
        if D == "Exponential"
        if c > aver
-       s = 1 - (1 - sp.loss_ratio / sp.expense_ratio) * leng / sp.duration * exp(-(1 / aver - leng / sp.duration / ( leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio)) * sp.initial_capital) / (1 / aver * leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio - leng / sp.duration);
+       s = 1 - (1 - sp.loss_ratio / sp.expense_ratio) * leng / sp.duration * exp(-(1 / aver - leng / sp.duration / ( leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio)) * u_0) / (1 / aver * leng / sp.duration * aver * sp.expense_ratio / sp.loss_ratio - leng / sp.duration);
        else c < aver
        println("Wrong information about loss ratio and expense ratio")
        end
