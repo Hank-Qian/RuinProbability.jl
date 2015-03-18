065779eddb6d60d3c0110585b5c697a7ea726737
Hank-Qian <joeyuehank@gmail.com>
2015-03-18 12:54:47 +1100

    Update Sundrop.jl

--- src/Sundrop.jl
+++ src/Sundrop.jl
@@ -1,7 +1,7 @@
-function Sundrop(sp::SurplusProcess)
+function Sundrop(sp::SurplusProcess,u_0::Numbers)
 Compose.compose(context(),
-        (context(1-SurPro(sp,Exp),1-SurPro(sp,Exp),0.1,0.1), circle(), fill("bisque")),
-        (context(1-SurPro(sp,FG),1-SurPro(sp,FG),0.1,0.1), circle(), fill("orange")),
-        (context(1-SurPro(sp,MixExp),1-SurPro(sp,MixExp),0.1,0.1), circle(), fill("black")), 
+        (context(1-SurPro(sp,u_0,Exp),1-SurPro(sp,u_0,Exp),0.1,0.1), circle(), fill("bisque")),
+        (context(1-SurPro(sp,u_0,FG),1-SurPro(sp,u_0,FG),0.1,0.1), circle(), fill("orange")),
+        (context(1-SurPro(sp,u_0,MixExp),1-SurPro(sp,u_0,MixExp),0.1,0.1), circle(), fill("black")), 
         context(), rectangle(), fill("tomato"))
 end
